1159e7e538d41f9e42257dc91322889fcb84d21e
Muhamad Rifda Firdaus. S <102627725+murfs1@users.noreply.github.com>
2022-10-08 20:42:35 +0700

    Add files via upload

---
+++ tugasno2.py
@@ -0,0 +1,9 @@
+#tugas no. 2 membuat komentar nama, no. telepon dan nim
+#dikarenakan tidak perlu adanya inputan, langsung membuat ouput
+
+#output nama
+print("Muhamad Rifda Firdaus. S")
+#output no hp
+print("085795040693")
+#output nim
+print("20220040211")
---
+++ tugasno4(volumekubus).py
@@ -0,0 +1,11 @@
+#tugas no. 4 membuat program menghitung keliling lingkaran dan volume kubus
+#program menghitung volume kubus
+
+#membuatt inputan sisi berupa float
+sisi = float(input(" Masukkan Sisi : "))
+
+#membuat rumus volume kubus
+hasil = sisi+sisi+sisi
+
+#membuat output
+print(" Volume Kubus adalah : ",hasil)
